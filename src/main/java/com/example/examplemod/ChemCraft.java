3ca57d1d49ee80e012fa3dcd27ad5566e1e2f5df
Lucas Engleder <lucasengleder@gmail.com>
2019-06-25 16:39:29 +0200

    With .idea

--- src/main/java/com/example/examplemod/ChemCraft.java
+++ src/main/java/com/example/examplemod/ChemCraft.java
@@ -19,13 +19,15 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+
+@Mod("chemcraft " + ChemCraft.version)
+public class ChemCraft
 {
+    public static final String version = "1.0";
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public ChemCraft() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
